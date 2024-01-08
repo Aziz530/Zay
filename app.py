e43ba77104d734658dcf868d1f1be837c8716824
Maaroufi <aziz.Maaroufi@gnoc.corp>
2024-01-08 12:42:32 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='templates', static_url_path='')
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7071)
